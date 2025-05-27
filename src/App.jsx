b24f80afa928add1f44801892bc0368787f125a8
Rodrigo Moreira <roke3tres@gmail.com>
2025-05-27 16:55:35 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// src/App.jsx
+import { useRef } from 'react';
+import './App.css';
+
+function App() {
+  const audioRef = useRef(null);
+
+  const handlePlayAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  return (
+    <div className="container" onClick={handlePlayAudio}>
+      <img src="/fondo.jpg" alt="Fondo" className="fullscreen-image" />
+      <audio ref={audioRef} src="/audio.mp3" />
+    </div>
+  );
+}
+
+export default App;
